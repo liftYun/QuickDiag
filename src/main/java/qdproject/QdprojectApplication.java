57bbc9fb9c854e419a212a0c944d8d8fbd9e8117
liftyun <idoyun@idoyun-ui-MacBookPro.local>
2024-03-20 18:41:07 +0900

    first commit

---
+++ src/main/java/qdproject/QdprojectApplication.java
@@ -0,0 +1,13 @@
+package qdproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QdprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QdprojectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/qdproject/QdprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package qdproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QdprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
